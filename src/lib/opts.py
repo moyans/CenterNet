67f1409e701e23ab3d1096050c85efbcadd63d89
Moyan <ice_moyan@163.com>
2019-11-26 20:12:31 +0800

    merge coco class 80 to 1

--- src/lib/opts.py
+++ src/lib/opts.py
@@ -334,8 +334,10 @@ class opts(object):
     return opt
 
   def init(self, args=''):
+
     default_dataset_info = {
-      'ctdet': {'default_resolution': [512, 512], 'num_classes': 80, 
+      # merge coco 80 to 1
+      'ctdet': {'default_resolution': [512, 512], 'num_classes': 1, 
                 'mean': [0.408, 0.447, 0.470], 'std': [0.289, 0.274, 0.278],
                 'dataset': 'coco'},
       'exdet': {'default_resolution': [512, 512], 'num_classes': 80, 
