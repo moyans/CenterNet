a5986f146e5d4dff321708d78abf49362f16386b
Moyan <ice_moyan@163.com>
2019-11-28 14:15:07 +0800

    update code

--- experiments/ctdet_coco_dla_1x.sh
+++ experiments/ctdet_coco_dla_1x.sh
@@ -1,6 +1,7 @@
 cd src
 # train
-python main.py ctdet --exp_id coco_dla_1x --batch_size 128 --master_batch 9 --lr 5e-4 --gpus 0,1,2,3,4,5,6,7 --num_workers 16
+# python main.py ctdet --exp_id coco_dla_1x --batch_size 128 --master_batch 9 --lr 5e-4 --gpus 0,1,2,3,4,5,6,7 --num_workers 16
+python main.py ctdet --exp_id coco_dla_1x --batch_size 64 --master_batch 9 --lr 2.5e-4 --gpus 0,1,2,3 --num_workers 16
 # test
 python test.py ctdet --exp_id coco_dla_1x --keep_res --resume
 # flip test
